0daf984101ebd21123b0d1f62e767abe3c039c00
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-10-13 16:20:44 +0200

    es-14-log the data

---
+++ es-14-log data/challenge3.js
@@ -0,0 +1,15 @@
+// Challenge 3:
+//
+// Output a count value every time the importantTask function is called.
+// Reset the count value after 4 function calls.
+
+function importantTask() {
+  console.count("importantTaskCalled");
+}
+
+importantTask();
+importantTask();
+importantTask();
+importantTask();
+importantTask();
+importantTask();
