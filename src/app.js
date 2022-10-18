13f2cd29e3459f7a7473ff90d55f7bd5f19973f3
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-10-18 10:58:58 +0200

    fixed errors

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 require("express-async-errors");
 const client_1 = __importDefault(require("./lib/prisma/client"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 app.get("/planets", async (request, response) => {
     const planets = await client_1.default.planet.findMany();
     response.json(planets);
